25c3eaee9e2fcd081f69c48020a43b96dfde38b4
shao-fei <999@qq.com>
2020-12-19 18:06:15 +0800

    项目创建

---
+++ src/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	configureWwbpack: {
+		resolve: {
+			alias: {
+				'assets': '@/assets',
+				'common': '@/common',
+				'components': '@/components',
+				'network': '@/network',
+				'views': '@/views',
+
+			}
+		}
+	}
+}
